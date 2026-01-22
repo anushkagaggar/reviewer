28be8470676aac6024fd95110dbb02ddc66ae3d0
anushkagaggar <anumah23@gmail.com>
2026-01-22 19:49:54 +0530

    changed logs file for normalized version output logging

--- app/api/review.py
+++ app/api/review.py
@@ -46,7 +46,7 @@ def review_design(payload: ReviewInput):
     raw_output = llm.generate(full_prompt, "")
 
     print("\n===== RAW MODEL OUTPUT =====\n", raw_output)
-    with open("logs/raw_llm_outputs.txt", "a", encoding="utf-8") as f:
+    with open("logs/raw_llm_outputs_norm.txt", "a", encoding="utf-8") as f:
         f.write("\n\n==== NEW OUTPUT ====\n")
         f.write(raw_output)
 
